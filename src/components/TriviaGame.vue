c77c2254b9389ec4145504a2f07597f212234517
Grady Shelton <gradyish@gmail.com>
2019-06-21 15:21:11 -0700

    hook up deletion

--- src/components/TriviaGame.vue
+++ src/components/TriviaGame.vue
@@ -21,7 +21,9 @@ import Axios from "axios";
                             <td>{{ player.name }}</td>
                             <td>{{ player.answers }}</td>
                             <td>{{ player.points }}</td>
-                            <td>Action Buttons</td>
+                            <td>
+                                <button class="button is-primary" v-bind:class="{ 'is-loading' : isDeleting(player.id) }" @click="deletePlayer(player.id)">Delete Player</button>
+                            </td>
                         </tr>
                     </template>
                 </tbody>
@@ -33,6 +35,7 @@ import Axios from "axios";
 
 <script>
     import axios from 'axios'
+    import Vue from 'vue'
     import { API_BASE_URL } from '../config'
     import PlayerForm from './PlayerForm.vue'
 
@@ -62,7 +65,27 @@ import Axios from "axios";
         methods: {
             addPlayer(player) {
                 this.players.push(player)
+            },
+            isDeleting(id) {
+                let index = this.players.findIndex(player => player.id === id)
+                return this.players[index].isDeleting
+            },
+            async deletePlayer(id) {
+                let index = this.players.findIndex(player => player.id === id)
+                Vue.set(this.players[index], 'isDeleting', true)
+                await axios.delete(API_BASE_URL + '/players/' + id)
+                this.players.splice(index, 1)
             }
+            // isDeleting(id) {
+            // let index = this.players.findIndex(player => player.id === id)
+            // return this.players[index].isDeleting
+            // },
+            // async deletePlayer(id) {
+            //     let index = this.players.findIndex(player => player.id === id)
+            //     Vue.set(this.players[index], 'isDeleting', true)
+            //     await axios.delete(API_BASE_URL + '/players/' + id)
+            //     this.players.splice(index, 1)
+            // }
         }
     }
 </script>
